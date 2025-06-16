0366d2023911585acbeb91bc611eb87a9fe57f74
Luis Borja <139704941+lborja04@users.noreply.github.com>
2025-06-16 14:48:26 +0000

    guia terminada

---
+++ js/file01.js
@@ -0,0 +1,55 @@
+"use strict";
+
+import { fetchFakerData } from './functions.js';
+
+const loadData = async () => {
+
+    const url = 'https://fakerapi.it/api/v2/texts?_quantity=10&_characters=120';
+
+    try {
+        const result = await fetchFakerData(url);
+
+        if (result.success) {
+            console.log('Datos obtenidos con éxito:', result.body);
+        } else {
+            console.error('Error al obtener los datos:', result.error);
+        }
+
+    } catch (error) {
+
+        console.error('Ocurrió un error inesperado:', error);
+
+    }
+
+};
+
+(() => {
+    alert("¡Bienvenido a la página!");
+    console.log("Mensaje de bienvenida mostrado.");
+    loadData();
+})();
+
+const showToast = () => {
+    const toast = document.getElementById("toast-interactive");
+    if (toast) {
+        toast.classList.add("md:block");
+    }
+};
+
+(() => {
+    showToast();
+})();
+
+const showVideo = () => {
+    const demo = document.getElementById("demo");
+    if (demo) {
+        demo.addEventListener("click", () => {
+            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
+        });
+    }
+};
+
+(() => {
+    showToast();
+    showVideo();
+})();
